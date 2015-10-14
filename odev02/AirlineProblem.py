27f1bcd4b09877124d4a238c4667deb246dc05bb
ArdaIcmez <aicmez@gmail.com>
2015-10-14 20:22:25 +0300

    Writing the canRedeem function, reading from file done

--- odev02/AirlineProblem.py
+++ odev02/AirlineProblem.py
@@ -1,3 +1,34 @@
 from Airline import *
 
-myAirline = Airline(["arda","icmez","deneme"])
+def canRedeem(current,goal,pathForMiles,airlinesVisited,network) :
+    if current == goal:
+        pathForMiles.append(current)
+        return True 
+    elif current in airlinesVisited:
+        return False
+    else:
+        airlinesVisited.append(current)
+        pathForMiles.append(current)
+        
+        pos = -1
+        index = 0
+        while (pos ==-1 & index < len(network)):
+            if network[index].getName == current:
+                pos=index
+            index+=1
+        
+myFile = None
+try:
+    myFile = open('airlines.txt','r')
+except:
+    print "Opening failed"
+    exit
+airlinesPartnersNetwork = []
+pathForMiles = []
+airlinesVisited = []
+for myLine in myFile:
+    print myLine
+    airlineNames = myLine.split(',')
+    airlinesPartnersNetwork.append(Airline(airlineNames))
+start = raw_input("Enter airline miles are on: ")
+goal = raw_input("Enter goal airline: ")
