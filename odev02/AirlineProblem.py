8b0a48f06b1525bfd7080b975f072211afc36148
ArdaIcmez <aicmez@gmail.com>
2015-10-14 21:01:58 +0300

    Debugging finished, working, minor detail to tinker on the output

--- odev02/AirlineProblem.py
+++ odev02/AirlineProblem.py
@@ -9,11 +9,10 @@ def canRedeem(current,goal,pathForMiles,airlinesVisited,network) :
     else:
         airlinesVisited.append(current)
         pathForMiles.append(current)
-        
         pos = -1
         index = 0
-        while (pos ==-1 & index < len(network)):
-            if network[index].getName == current:
+        while (pos ==-1 and index < len(network)): 
+            if (network[index].getName() == current):
                 pos=index
             index+=1
         if pos == -1:
@@ -21,11 +20,11 @@ def canRedeem(current,goal,pathForMiles,airlinesVisited,network) :
         index=0
         partners = network[pos].getPartners()
         foundPath = False
-        while (not foundPath & index < len(network)):
+        while (not foundPath and index < len(partners)):
             foundPath = canRedeem(partners[index], goal, pathForMiles, airlinesVisited, network)
             index+=1
         if not foundPath:
-            pathForMiles.remove(len(pathForMiles)-1)
+            del pathForMiles[len(pathForMiles)-1]
         return foundPath
 myFile = None
 try:
@@ -39,6 +38,11 @@ airlinesVisited = []
 for myLine in myFile:
     print myLine
     airlineNames = myLine.split(',')
+    airlineNames[-1] = airlineNames[-1].replace('\n','')
     airlinesPartnersNetwork.append(Airline(airlineNames))
 start = raw_input("Enter airline miles are on: ")
 goal = raw_input("Enter goal airline: ")
+if canRedeem(start,goal,pathForMiles,airlinesVisited,airlinesPartnersNetwork):
+    print "Path to redeem miles: ", pathForMiles
+else:
+    print "Cannot convert miles from " , start , " to " , goal , "."
