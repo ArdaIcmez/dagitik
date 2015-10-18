c91990f6fb06e55c9887eb11731a8e31ae29b475
ArdaIcmez <aicmez@gmail.com>
2015-10-18 21:46:19 +0300

    Part with Threads finished, added another implementation using Queue, didn't see that much of a difference with the first method.

--- odev03/caesar_threadQUEUE.py
+++ odev03/caesar_threadQUEUE.py
@@ -1,16 +1,17 @@
-import sys, profile, threading
+import sys, profile, threading, Queue, time
 
-inputLock = threading.Lock()
+queueLock = threading.Lock()
 outputLock = threading.Lock()
 alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 class myThread (threading.Thread):
     outputOrder = 0
-    def __init__(self, threadID, name, order):
+    def __init__(self, threadID, name, order,q):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
         self.order = order
+        self.q = q
     def run(self):
         readFile(self)
 
@@ -32,28 +33,29 @@ def encryptBlock(text):
     return encryptedText
 
 def readFile(thread):
-    while True:
-        inputLock.acquire()
-        myText = myInputFile.read(configParam[2])
-        if myText == "":
-            inputLock.release()
-            break
-        thread.order = myThread.outputOrder
-        myThread.outputOrder+=1
-        inputLock.release()
-        
-        myText = encryptBlock(myText);
-
-        outputLock.acquire()
-        myOutputFile.seek(thread.order*configParam[2])
-        myOutputFile.write(myText)
-        outputLock.release()
+    while not exitFlag:
+        queueLock.acquire()
+        if not workQueue.empty():
+            data = thread.q.get()
+            thread.order = myThread.outputOrder
+            myThread.outputOrder+=1
+            queueLock.release()
+            data = encryptBlock(data);
+            outputLock.acquire()
+            myOutputFile.seek(thread.order*configParam[2])
+            myOutputFile.write(data)
+            outputLock.release()
+        else:
+            queueLock.release()
     
 def main():
     global keyAlphabet
     global configParam
+    global workQueue
+    global exitFlag
+    exitFlag=0
     configParam = []
-    
+    workQueue = Queue.Queue(10)
     if len(sys.argv) == 4:
         for e in range(1,4):
             if sys.argv[e].isdigit():
@@ -74,10 +76,23 @@ def main():
     
     myThreads = []
     keyAlphabet = createKey()
+    
+    queueLock.acquire()
+    myText = myInputFile.read(configParam[2])
+    while myText != "":
+        workQueue.put(myText)
+        myText = myInputFile.read(configParam[2])
+    queueLock.release()
+    
     for i in range(0,configParam[1]):
-        thread = myThread(i,"Thread0"+`i`,0)
+        thread = myThread(i,"Thread0"+`i`,0,workQueue)
         thread.start()
         myThreads.append(thread)
+        
+    while not workQueue.empty():
+        pass
+    
+    exitFlag = 1
     for t in myThreads:
         t.join()
     myInputFile.close()
