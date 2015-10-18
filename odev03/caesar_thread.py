60ba6ddcb4e10b8a8d2ccf31c162f30f2a70e9c8
ArdaIcmez <aicmez@gmail.com>
2015-10-18 19:50:47 +0300

    Threads functional, need to fix encryption and implement output file

--- odev03/caesar_thread.py
+++ odev03/caesar_thread.py
@@ -1,4 +1,21 @@
-import sys , profile
+import sys, profile, threading
+
+inputLock = threading.Lock()
+outputLock = threading.Lock()
+alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+keyAlphabet = ""
+
+class myThread (threading.Thread):
+    outputOrder = 1 
+    def __init__(self, threadID, name, order,inputfile):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.order = order
+        self.inputfile = inputfile
+    def run(self):
+        readFile(self)
+
 def createKey(shift,alphabet):
     encryptedText = ""
     myLenght = len(alphabet)
@@ -6,6 +23,28 @@ def createKey(shift,alphabet):
         encryptedText += alphabet[(alphabet.index(i)-shift)%myLenght]
     return encryptedText
 
+def encryptBlock(text):
+    encryptedText = ""
+    text = text
+    for i in text:
+        if i in alphabet:
+            encryptedText += keyAlphabet[alphabet.index(i)]
+        else:
+            encryptedText += i
+    return encryptedText
+
+def readFile(thread):
+    while True:
+        myText = thread.inputfile.read(5)
+        if myText == "":
+            break
+        myText = encryptBlock(myText);
+        thread.order = myThread.outputOrder
+        myThread.outputOrder+=1
+        outputLock.acquire()
+        print "outpt",thread.order ,thread.threadID, myText
+        outputLock.release()
+    
 def main():
     configParam = []
     if len(sys.argv) == 4:
@@ -18,18 +57,25 @@ def main():
     else:
         print "usage : <filename> <shift> <numThread> <length> "
         sys.exit()
-    alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    keyAlphabet =  createKey(configParam[0],alphabet)
-    print keyAlphabet
-    """
-    myFile = None
     try:
         myFile = open('metin.txt','r')
     except:
         print "Opening failed"
         exit
-      """  
+        
+    inputCounter = 0
+    outputCounter = 0
     
+    myThreads = []
+    keyAlphabet =  createKey(configParam[0],alphabet)
+    print keyAlphabet
+    for i in range(0,configParam[1]):
+        thread = myThread(i,"Thread0"+'i',0,myFile)
+        thread.start()
+        myThreads.append(thread)
+    for t in myThreads:
+        t.join()
 #myfile.close()
 if __name__ == '__main__':
-    profile.run('print main(); print')
+    main()
+    #profile.run('print main(); print')
