001706b2c1f908b34263dcfc592e215a8a4f258f
ArdaIcmez <aicmez@gmail.com>
2015-10-18 17:38:15 +0300

    Implemented profile to see thread performance

--- odev03/caesar_thread.py
+++ odev03/caesar_thread.py
@@ -1,26 +1,35 @@
-import sys
-
+import sys , profile
+def createKey(shift,alphabet):
+    encryptedText = ""
+    myLenght = len(alphabet)
+    for i in alphabet:
+        encryptedText += alphabet[(alphabet.index(i)-shift)%myLenght]
+    return encryptedText
 
 def main():
     configParam = []
     if len(sys.argv) == 4:
         for e in range(1,4):
             if sys.argv[e].isdigit():
-                configParam.append(sys.argv[e])
+                configParam.append(int(sys.argv[e]))
             else:
                 print "parameters other than filename have to be integers"
                 sys.exit()
     else:
         print "usage : <filename> <shift> <numThread> <length> "
         sys.exit()
-    for x in configParam:
-        print x
-    
+    alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    keyAlphabet =  createKey(configParam[0],alphabet)
+    print keyAlphabet
+    """
     myFile = None
     try:
         myFile = open('metin.txt','r')
     except:
         print "Opening failed"
         exit
+      """  
+    
+#myfile.close()
 if __name__ == '__main__':
-    main()
+    profile.run('print main(); print')
