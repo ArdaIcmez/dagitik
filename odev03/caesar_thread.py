b7867c628803524a6d99158efee9e420ff570532
ArdaIcmez <aicmez@gmail.com>
2015-10-18 20:12:48 +0300

    Implemented encryption, working on the output

--- odev03/caesar_thread.py
+++ odev03/caesar_thread.py
@@ -3,7 +3,6 @@ import sys, profile, threading
 inputLock = threading.Lock()
 outputLock = threading.Lock()
 alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-keyAlphabet = ""
 
 class myThread (threading.Thread):
     outputOrder = 1 
@@ -25,7 +24,7 @@ def createKey(shift,alphabet):
 
 def encryptBlock(text):
     encryptedText = ""
-    text = text
+    text = text.upper()
     for i in text:
         if i in alphabet:
             encryptedText += keyAlphabet[alphabet.index(i)]
@@ -47,6 +46,7 @@ def readFile(thread):
     
 def main():
     configParam = []
+    global keyAlphabet
     if len(sys.argv) == 4:
         for e in range(1,4):
             if sys.argv[e].isdigit():
@@ -58,24 +58,23 @@ def main():
         print "usage : <filename> <shift> <numThread> <length> "
         sys.exit()
     try:
-        myFile = open('metin.txt','r')
+        global myInputFile,myOutputFile
+        myInputFile = open('metin.txt','r')
+        myOutputFile = open('crypted_'+`configParam[0]`+'_'+`configParam[1]`+'_'+`configParam[2]`,'w')
     except:
         print "Opening failed"
-        exit
-        
-    inputCounter = 0
-    outputCounter = 0
+        sys.exit()
     
     myThreads = []
-    keyAlphabet =  createKey(configParam[0],alphabet)
-    print keyAlphabet
+    keyAlphabet = createKey(configParam[0],alphabet)
     for i in range(0,configParam[1]):
-        thread = myThread(i,"Thread0"+'i',0,myFile)
+        thread = myThread(i,"Thread0"+`i`,0,myInputFile)
         thread.start()
         myThreads.append(thread)
     for t in myThreads:
         t.join()
-#myfile.close()
+    myFile.close()
+    myOutputFile.close()
 if __name__ == '__main__':
     main()
     #profile.run('print main(); print')
