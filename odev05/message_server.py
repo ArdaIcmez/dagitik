5f6da350604d579ce2c8154d9ea6458835a0434d
ArdaIcmez <aicmez@gmail.com>
2015-11-09 18:36:43 +0200

    Changed things, getting ready to test

--- odev05/message_server.py
+++ odev05/message_server.py
@@ -1,6 +1,8 @@
-import Queue,time
+import Queue
+import time
 import socket
-import sys, threading
+import sys
+import threading
 class WriteThread (threading.Thread):
     def __init__(self, name, cSocket, address, threadQueue, logQueue ):
         threading.Thread.__init__(self)
@@ -10,21 +12,30 @@ class WriteThread (threading.Thread):
         self.lQueue = logQueue
         self.tQueue = threadQueue
     def run(self):
+        exitFlag = 0
         self.lQueue.put("Starting " + self.name)
-        while True:
-        # burasi kuyrukta sirasi gelen mesajlari
-        # gondermek icin kullanilacak
+        while not exitFlag:
             if self.threadQueue.qsize() > 0:
+                
                 queue_message = self.threadQueue.get()
-            # gonderilen ozel mesajsa
+                
+                # gonderilen ozel mesajsa
                 if queue_message[0]:
                     message_to_send = "MSG " + queue_message[2]
+                    
                 # genel mesajsa
                 elif queue_message[1]:
                     message_to_send = "SAY " + queue_message[2]
+                    
                 # hicbiri degilse sistem mesajidir
                 else:
-                    message_to_send = "SYS " + queue_message[2]
+                    if queue_message[2][0:3] == "BYE":
+                        exitFlag=1
+                    message_to_send = queue_message[2]
+                    
+                self.cSocket.send(message_to_send)
+        
+        self.cSocket.close()        
         self.lQueue.put("Exiting " + self.name)
         
 class ReadThread (threading.Thread):
@@ -36,97 +47,112 @@ class ReadThread (threading.Thread):
         self.lQueue = logQueue
         self.fihrist = fihrist
         self.tQueue = threadQueue
-    def csend(self,response):
-        self.cSocket.send(response)
     def parser(self, data):
         global queueLock
         data = data.strip('\n')
+        
         # henuz login olmadiysa
         if not self.nickname and not data[0:3] == "USR":
             response  = "ERL"
-            self.csend(response)
+            self.threadQueue.put((None,None,response))
             return 0
-        """if data[3] != " ":
-            response = "ERR"
-            self.csend(response)
-            return 0"""
+        
         if data[0:3] == "USR":
             nickname = data[4:]
+            
             # kullanici yoksa
             if nickname not in self.fihrist.keys():
                 self.nickname = nickname
                 response = "HEL " + nickname
-                self.csend(response)
+                self.threadQueue.put((None,None,response))
+                
                 # fihristi guncelle
                 self.fihrist[nickname] = self.tQueue
+                
                 # tell users that another person has joined to chat
                 queueLock.acquire()
                 for key in self.fihrist.keys():
-                    self.fihrist[key].put((None,None,self.nickname+" has joined the chat"))
+                    self.fihrist[key].put((None,None,"SYS "+self.nickname+" has joined the chat"))
                 queueLock.release()
+                
                 self.lQueue.put(self.nickname + " has joined.")
                 return 0
+            
             else:
                 # kullanici reddedilecek
                 response = "REJ " + nickname
-                self.csend(response)
-                # baglantiyi kapat
-                self.cSocket.close()
+                self.threadQueue.put((None,None,response))
                 return 1
+            
         elif data[0:3] == "QUI":
             response = "BYE " + self.nickname
-            self.csend(response)
+            self.threadQueue.put((None,None,response))
+            
             # notify other users
             queueLock.acquire()
             for key in self.fihrist.keys():
-                self.fihrist[key].put((None,None,self.nickname+" has left the chat"))
-                queueLock.release()
+                self.fihrist[key].put((None,None,"SYS "+self.nickname+" has left the chat"))
+            queueLock.release()
+            
             # fihristten sil
             del self.fihrist[self.nickname]
             # log gonder
             self.lQueue.put(self.nickname + " has left.")
-            # baglantiyi sil
-            self.cSocket.close()
             return 1
+        
         elif data[0:3] == "LSQ":
             response = "LSA "
+            
             for key in sorted(self.fihrist.keys()):
-                response += key
-            self.csend(response)
+                response += key+":"
+                
+            response = response[:-1]
+            self.threadQueue.put((None,None,response))
             return 0
+        
         elif data[0:3] == "TIC":
             response = "BALTAZOR"
             self.csend(response)
             return 0
+        
         elif data[0:3] == "SAY":
             response = "SOK"
-            self.csend(response)
+            self.threadQueue.put((None,None,response))
             messageAll = data[4:]
+            
             queueLock.acquire()
             for key in self.fihrist.keys():
                 self.fihrist[key].put((None,self.nickname,messageAll))
             queueLock.release()
+            
             return 0
+        
         elif data[0:3] == "MSG":
             restMessage = data[4:].split(':',1)
             to_nickname=restMessage[0]
             message = restMessage[1]
+            
             if to_nickname not in self.fihrist.keys():
-                response = "MNO"
+                response = "MNO" + to_nickname
+                
             else:
                 queue_message = (to_nickname, self.nickname, message)
+                
                 # gonderilecek threadQueueyu fihristten alip icine yaz
                 queueLock.acquire()
                 self.fihrist[to_nickname].put(queue_message)
                 queueLock.release()
+                
                 response = "MOK"
-            self.csend(response)
+            self.threadQueue.put((None,None,response))
             return 0
+        
         else:
         # bir seye uymadiysa protokol hatasi verilecek
             response = "ERR"
-            self.csend(response)
+            self.threadQueue.put((None,None,response))
             return 0
+        
     def run(self):
         self.lQueue.put("Starting " + self.name)
         while True:
